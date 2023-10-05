1e40fe6fa562740b2a57f753486c17c4c754a3a1
midhun-coddletech <midhun.o@coddletech.com>
2023-10-05 16:50:22 +0530

    updated code updateproducts and deleteproducts api

--- src/admin/controllers/products.js
+++ src/admin/controllers/products.js
@@ -1,7 +1,7 @@
 /* eslint-disable linebreak-style */
 /* eslint-disable max-len */
 /* eslint-disable no-console */
-const adminModel = require('../models/admin');
+const adminModel = require('../models/products');
 
 async function addProduct(req, res) {
     try {
@@ -20,4 +20,36 @@ async function addProduct(req, res) {
     }
 }
 
-module.exports = { addProduct };
+async function updateProduct(req, res) {
+    try {
+        const productId = req.params.id;
+        const updatedDetails = req.body;
+        console.log(updatedDetails, productId);
+        const result = await adminModel.updateProduct(updatedDetails, productId);
+        if (result) {
+            res.status(200).json({ message: 'Item updated successfully' });
+        } else {
+            res.status(404).json({ message: 'Something went wrong' });
+        }
+    } catch (err) {
+        console.error('DB retrieve error', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
+async function deleteProduct(req, res) {
+    try {
+        const productId = req.params.id;
+        const result = await adminModel.deleteProduct(productId);
+        if (result) {
+            res.status(200).json({ message: 'Item deleted successfully' });
+        } else {
+            res.status(404).json({ message: 'Something went wrong' });
+        }
+    } catch (err) {
+        console.error('DB retrieve error', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
+module.exports = { addProduct, updateProduct, deleteProduct };
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -8,5 +8,7 @@ const adminMiddleware = require('../middleware/auth');
 
 router.post('/login', authController.adminLogin);
 router.post('/addproduct', adminMiddleware.verifyToken, adminController.addProduct);
+router.put('/updateproducts/:id', adminMiddleware.verifyToken, adminController.updateProduct);
+router.delete('/deleteproduct/:id', adminMiddleware.verifyToken, adminController.deleteProduct);
 
 module.exports = router;
