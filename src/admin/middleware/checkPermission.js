2638f4db33cffb6e74a184ef240ca5d367fb6459
midhun-coddletech <midhun.o@coddletech.com>
2023-10-06 13:54:45 +0530

    updated code

--- src/admin/middleware/checkPermission.js
+++ src/admin/middleware/checkPermission.js
@@ -3,7 +3,30 @@
 const jwt = require('jsonwebtoken');
 const authModel = require('../models/auth');
 
-async function checkAdminPermission(req, res, next) {
+async function checkEditAccess(req, res, next) {
+    try {
+        const authHeader = req.headers.authorization;
+        const secretKey = process.env.JWT_SECRET_KEY;
+        if (!authHeader) {
+            res.json({ error: 'No token provided' });
+        } else {
+            const token = authHeader.split(' ')[1];
+            const userDetails = jwt.verify(token, secretKey);
+            const userId = userDetails.id.id;
+            const userRoleId = await authModel.findUserRole(userId);
+            console.log(userRoleId);
+            if (userRoleId === 1 || userRoleId === 3) {
+                next();
+            } else {
+                res.json({ error: 'You dont have access to Edit Items' });
+            }
+        }
+    } catch (err) {
+        res.status(401).json({ error: 'Token verification failed' });
+    }
+}
+
+async function checkAddAccess(req, res, next) {
     try {
         const authHeader = req.headers.authorization;
         const secretKey = process.env.JWT_SECRET_KEY;
@@ -18,7 +41,7 @@ async function checkAdminPermission(req, res, next) {
             if (userRoleId === 1 || userRoleId === 2) {
                 next();
             } else {
-                res.json({ error: 'You dont have access' });
+                res.json({ error: 'You dont have access to Add Items' });
             }
         }
     } catch (err) {
@@ -26,4 +49,27 @@ async function checkAdminPermission(req, res, next) {
     }
 }
 
-module.exports = { checkAdminPermission };
+async function checkDeleteAccess(req, res, next) {
+    try {
+        const authHeader = req.headers.authorization;
+        const secretKey = process.env.JWT_SECRET_KEY;
+        if (!authHeader) {
+            res.json({ error: 'No token provided' });
+        } else {
+            const token = authHeader.split(' ')[1];
+            const userDetails = jwt.verify(token, secretKey);
+            const userId = userDetails.id.id;
+            const userRoleId = await authModel.findUserRole(userId);
+            console.log(userRoleId);
+            if (userRoleId === 1 || userRoleId === 4) {
+                next();
+            } else {
+                res.json({ error: 'You dont have access to delete' });
+            }
+        }
+    } catch (err) {
+        res.status(401).json({ error: 'Token verification failed' });
+    }
+}
+
+module.exports = { checkEditAccess, checkAddAccess, checkDeleteAccess };
