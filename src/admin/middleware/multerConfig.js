d81e74d4a13c0b572b1b80911a1e6dda719bf6b6
midhun-coddletech <midhun.o@coddletech.com>
2023-10-09 11:17:28 +0530

    file type check for image

--- src/admin/middleware/multerConfig.js
+++ src/admin/middleware/multerConfig.js
@@ -1,5 +1,17 @@
 /* eslint-disable linebreak-style */
 const multer = require('multer');
+const path = require('path');
+
+const fileFilter = (req, file, cb) => {
+    const fileTypes = ['.jpg', '.jpeg'];
+    const extensionName = path.extname(file.originalname).toLowerCase();
+
+    if (fileTypes.includes(extensionName)) {
+        cb(null, true);
+    } else {
+        cb(new Error('Please upload file with jpg or jpeg format'), false);
+    }
+};
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -11,6 +23,6 @@ const storage = multer.diskStorage({
     },
 });
 
-const upload = multer({ storage, limits: { fileSize: 2097152 } });
+const upload = multer({ storage, limits: { fileSize: 2097152 }, fileFilter });
 
 module.exports = { upload };
