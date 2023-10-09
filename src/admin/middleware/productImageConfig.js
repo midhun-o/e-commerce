62925e0d921b110e0365d79a46b30a89eb2bede3
midhun-coddletech <midhun.o@coddletech.com>
2023-10-09 15:03:03 +0530

    added banner image feature

---
+++ src/admin/middleware/productImageConfig.js
@@ -0,0 +1,28 @@
+/* eslint-disable linebreak-style */
+const multer = require('multer');
+const path = require('path');
+
+const fileFilter = (req, file, cb) => {
+    const fileTypes = ['.jpg', '.jpeg'];
+    const extensionName = path.extname(file.originalname).toLowerCase();
+
+    if (fileTypes.includes(extensionName)) {
+        cb(null, true);
+    } else {
+        cb(new Error('Please upload file with jpg or jpeg format'), false);
+    }
+};
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'src/public/img/');
+    },
+    filename: function (req, file, cb) {
+        const fileName = Date.now() + file.originalname.replace(' ', '-');
+        cb(null, fileName);
+    },
+});
+
+const productImageUpload = multer({ storage, limits: { fileSize: 2097152 }, fileFilter });
+
+module.exports = { productImageUpload };
