2687caae374defd980166ced2970a2d7752174eb
Midhun O <136676213+midhun-o@users.noreply.github.com>
2024-12-03 12:48:52 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,30 +1,22 @@
-/* eslint-disable linebreak-style */
-
 const jwt = require('jsonwebtoken');
 
 function verifyToken(req, res, next) {
     try {
         const authHeader = req.headers.authorization;
         const secretKey = process.env.JWT_SECRET_KEY;
+
         if (!authHeader) {
-            res.status(401).json({ success: false, error: 'No token provided' });
-        } else {
-            const token = authHeader.split(' ')[1];
-            const userData = jwt.verify(token, secretKey, (err, data) => {
-                if (err) {
-                    res.status(401).json({ success: false, error: 'Wrong Token' });
-                } else {
-                    return data;
-                }
-            });
-            if (userData) {
-                const userId = userData.id.id;
-                req.userId = userId;
-                next();
-            }
+            return res.status(401).json({ success: false, error: 'No token provided' });
         }
+
+        const token = authHeader.split(' ')[1];
+
+        const userData = jwt.verify(token, secretKey);
+
+        req.userId = userData.id;
+        next(); 
     } catch (err) {
-        return false;
+        return res.status(401).json({ success: false, error: 'Invalid or expired token' });
     }
 }
 
